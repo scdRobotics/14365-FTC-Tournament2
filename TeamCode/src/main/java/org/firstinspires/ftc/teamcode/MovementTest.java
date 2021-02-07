b2b348173c1ca44b6016e05e0e075fd1ddcaa392
scdRobotics <logandavid555@gmail.com>
2021-02-07 15:16:25 -0500

    2/7/21 3:15 Adjusted MovementTest.java- functional again
    Signed-off-by: scdRobotics <logandavid555@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementTest.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 // TODO: Test & fix values, add wobble grab support
 
-@Autonomous(name="MovementTest", group="linearOpMode")
+@Autonomous(name="MovementTestAndroidStudio", group="linearOpMode")
 public class MovementTest extends AutonomousPrimeTest{
     @Override
     public void runOpMode(){
@@ -13,52 +13,59 @@ public class MovementTest extends AutonomousPrimeTest{
         waitForStart();
 
         //launch speed start = 0.42
-		//launch speed second = 0.465
-		//launch function is launchAdvanceFast
+        //launch speed second = 0.465
+        //launch function is launchAdvanceFast
 
         double version = 0;
 
-		//Version 0 is 4 rings
-		//Version 1 is 1 ring
-		//Version 2 is 0 rings
+        //Version 0 is 4 rings
+        //Version 1 is 1 ring
+        //Version 2 is 0 rings
 
         if(version==0){
+            wobbleLock();
             startLaunch(0.42);
             forwardEncoder(160, 1);
             zeroBotEncoder(1);
             pause(0.2);
             launchAdvanceFast();
+            launchAdvanceFast();
             strafeLeftEncoder(25, 1);
             zeroBotEncoder(1);
             pause(0.2);
             launchAdvanceFast();
+            //launchAdvanceFast();
             strafeLeftEncoder(20, 1);
             zeroBotEncoder(1);
             pause(0.2);
             launchAdvanceFast();
             pause(0.2);
 
+            intakeStart(1);
             startLaunch(0.465);
             strafeRightEncoder(87, 1);
+            zeroBotEncoder(1);
             pause(0.2);
-            reverseEncoder(45, 1);
+            reverseEncoder(45, 0.4);
             pause(0.5);
             launchAdvanceFast();
-            reverseEncoder(7, 1);
+            reverseEncoder(9, 0.5);
             pause(0.5);
             launchAdvanceFast();
-            reverseEncoder(7, 1);
+            reverseEncoder(9, 0.5);
             pause(0.5);
             launchAdvanceFast ();
-            reverseEncoder(7, 1);
+            reverseEncoder(9, 0.5);
             pause(0.5);
             launchAdvanceFast();
+            pause(0.5);
             launchAdvanceFast();
-            pause(0.2);
+            pause(0.5);
+            intakeEnd();
 
-            rightEncoder(0.25, 1);
+            rightEncoder(0.225, 1);
             pause(0.2);
-            forwardEncoder(180, 1);
+            forwardEncoder(210, 1);
             wobbleRelease();
             pause(0.25);
             reverseEncoder(95, 1);
